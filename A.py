0caeaf9894304e8794cb56525964bd8e3f7cea5c
takakikato <37848247+takakikato@users.noreply.github.com>
2020-05-02 08:48:14 +0630

    added and improved functions

--- A.py
+++ A.py
@@ -1,33 +1,96 @@
 import requests
 from pprint import pprint
 
+
 def get_json(url, parameters):
     '''
+    url: string
+    parameters: dictionary
+
+    Given a properly formatted url and parameters for a JSON web API request, 
+    return a Python JSON object containing the response to that request.
     '''
     return requests.get(url, params=parameters).json()
 
 
 def post_json(url, parameteres, json, header):
     '''
+    url: string
+    parameters: dictionary
+    json: JSON object / dictionary
+    header: dictionary
+
+    Given a properly formatted url, parameters, json, and header for a JSON web API request, 
+    return a Python JSON object containing the response to that request.
     '''
     return requests.post(url, params=parameteres, json=json, headers=header).json()
 
 
-def get_recipe(ingredient):
+def get_recipe(ingredient, end, start = 0):
     '''
+    ingredient: string
+    end: integer
+    start: integer
+
+    Given a string containing ingredients, return JSON object containing the recipes that include given ingredients.
+    Retrun number of recipes between start and end.
     '''
-    parameters = {'app_id': 'ef73e741', 'app_key': '687e39d9619e78a7956d715af8585b7b', 'q': ingredient, 'to': 3}
+    parameters = {'app_id': 'ef73e741', 'app_key': '687e39d9619e78a7956d715af8585b7b', 'q': ingredient, 'from': start, 'to': end}
     url = 'https://api.edamam.com/search'
 
-    response = get_json(url, parameters)['hits']
-    for item in response:
-        print(item['recipe']['label'])
-        print(item['recipe']['ingredientLines'])
-        print('\n')
+    return get_json(url, parameters)['hits']
 
+
+def print_recipe(ingredient, end):
+    '''
+    ingredient: string
+    end: integer
+
+    Given a string containing ingredients, prints properly formatted recipes that include given ingredients.
+    Retruns end numbers of recipes.
+    '''
+    response = get_recipe(ingredient, end)
+
+    for i, recipe in enumerate(response):
+        print(f"{i+1}. {recipe['recipe']['label']} ({recipe['recipe']['calories']:.2f} cal)")
+        print(f"Caution: {', '.join(recipe['recipe']['cautions'])} | Diet: {'. '.join(recipe['recipe']['dietLabels'])} | Health: {', '.join(recipe['recipe']['healthLabels'])}")
+        print("Ingredients:")
+        for ingredient in recipe['recipe']['ingredientLines']:
+            print(f'  \u2022 {ingredient}')
+        print(recipe['recipe']['image'])
+        print('\n')     
+
+
+def print_recipe_nutrition(ingredient, choice):
+    '''
+    ingredient: string
+    choice: integer
+
+    Given a string containing ingredients, prints properly formatted nutritional facts for chosen recipe.
+    '''
+    response = get_recipe(ingredient, choice, choice-1)
+        
+    print(f"{choice}. {response[0]['recipe']['label']:<44} Calories {response[0]['recipe']['calories']:>7.2f}")
+    print('----------------------------------------------------------------')
+    print('Nutrient                              Total        % Daily Value')
+    print('----------------------------------------------------------------')
+    for nutrient in response[0]['recipe']['digest']:
+        print(f"{nutrient['label']:<30} {nutrient['total']:>10.2f} {nutrient['unit']:<5}{nutrient['daily']:>15.2f} %")
+        if 'sub' in nutrient:
+            for sub_nutrient in nutrient['sub']:
+                print(f"  {sub_nutrient['label']:<28} {sub_nutrient['total']:>10.2f} {sub_nutrient['unit']:<5}{sub_nutrient['daily']:>15.2f} %")  
+        if nutrient['label'] == 'Protein':
+            print('----------------------------------------------------------------')
+    print('----------------------------------------------------------------')
+    # pprint(response[0]['recipe']['digest'][0]['sub'][0])
+    
 
 def get_nutrition(title, ingredient):
     '''
+    title: string
+    ingr: list
+
+    Given title of the recipe, and list of ingredients, returns JSON object containing nutritional fact of the recipe.
     '''
     parameteres = {'app_id': '366257d2', 'app_key': 'd01df5907c09a9b04ba0dd4c0d0ef652'}
     json = {'title': title, 'ingr': ingredient}
@@ -36,12 +99,46 @@ def get_nutrition(title, ingredient):
 
     return post_json(url, parameteres, json, header)
 
+
+def print_nutrition(title, ingredient):
+    '''
+    title: string
+    ingredient: list
+
+    Given title of the recipe and list of ingredients, prints properly formatted nutrional facts of the recipe
+    '''
+
+    response = get_nutrition(title, ingredient)
+
+    daily = response['totalDaily']
+    total = response['totalNutrients']
+
+    print(title)
+    print(f"Caution: {', '.join(response['cautions'])} | Diet: {'. '.join(response['dietLabels'])} | Health: {', '.join(response['healthLabels']).replace('_', '-')}")
+    print('Ingredients:')
+    for item in ingredient:
+        print(f'  \u2022 {item}')   
+    print('\n')
+    print('Nutrient                              Total        % Daily Value')
+    print('----------------------------------------------------------------')
+    for nutrient in daily:
+        print(f"{daily[nutrient]['label']:<30} {total[nutrient]['quantity']:>10.2f} {total[nutrient]['unit']:<5}{daily[nutrient]['quantity']:>15.2f} %")
+        if daily[nutrient]['label'] == 'Energy' or daily[nutrient]['label'] == 'Protein':
+            print('----------------------------------------------------------------')
+    print('----------------------------------------------------------------')
+
+
 def main():
-    get_recipe('salmon')
-    title = 'Steamed Salmon'
-    ingredient = ['1 center-cut boneless salmon fillet (4 ounces), skin on', 'Lemon wedge, for serving']
-    pprint(get_nutrition(title, ingredient))
+    print_recipe('Fried Rice', 3)
+    print_recipe_nutrition('Fried Rice', 3)
+
+    print('\n')
+
+    title = 'Takaki Fried Rice'
+    ingredient = ['1 cup rice', '5 shrimps', '2 eggs', 'half carrot', '150g chicken ']
+    print_nutrition(title, ingredient)
 
 
 if __name__ == "__main__":
     main()
+
