89db38625a923d2f0f42cf09640ba8a04d69299f
ikim1 <60156429+ikim1@users.noreply.github.com>
2020-05-01 21:09:59 -0700

    update

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, request
 import requests
-from A import get_recipe, get_nutrition
+from A import get_recipe, get_nutrition, print_recipe_nutrition
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def welcome():
-    return render_template("welcome.html")
+    return render_template('welcome.html')
 
 @app.route("/search/", methods=["GET", "POST"])
-def search(list):
+def recipe_search():
     if request.method == "POST":
-       pass
+        ingredient = str(request.form["ingredient"])
+        ingredient,  = get_recipe(location)
+
+
+        if station_name:
+            return render_template("results.html", )
+        else:
+            return render_template("results.html", error=True)
+
+    return render_template("results.html", error=None)
