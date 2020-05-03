5b59ce3c695e2da425c9e17a990faec1960e792b
ikim1 <60156429+ikim1@users.noreply.github.com>
2020-05-02 17:41:23 -0700

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask
+from flask import request
+from flask import render_template
 from A import get_recipe, get_nutrition, print_recipe_nutrition, get_recipe2, get_recipe_url
 
 app = Flask(__name__)
@@ -13,9 +14,12 @@ def recipe_search():
     if request.method == "POST":
         ingredient = str(request.form["ingredient"])
         recipes  = get_recipe2(ingredient, 3)
+        label_1 = recipes[0]['label']
+        calories_1 = recipes[0]['calories']
         url = get_recipe_url(ingredient, 3)
+        url_1 = url[0]
         if recipes:
-            return render_template("results.html", url=url, recipe=recipe)
+            return render_template("results.html", url=url, calories_1=calories_1, label_1=label_1)
         else:
             return render_template("results.html", error=True)
 
